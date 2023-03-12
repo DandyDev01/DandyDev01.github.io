f157c9644c6b79a5d992a35da6a2433f7a15de29
Kelsey <37933054+kelman7000@users.noreply.github.com>
2023-03-11 19:05:29 -0700

    added hamburger menu

--- scripts/jquery.js
+++ scripts/jquery.js
@@ -2,26 +2,35 @@
 
 $(document).ready(function(){
     $(".img-container img").on("click", function() {
-        if($("#overlay").hasClass("enable")) {
-            if($(this).attr("src") == $("#overlay-img").attr("src")) {
-                $("#overlay").addClass("disable");
-                $("#overlay").removeClass("enable");
-            }
-            else {
-                var src = $(this).attr("src");
-                $("#overlay-img").attr("src", src);
-            }
+      if($("#overlay").hasClass("enable")) {
+        if($(this).attr("src") == $("#overlay-img").attr("src")) {
+          $("#overlay").addClass("disable");
+          $("#overlay").removeClass("enable");
         }
         else {
-            $("#overlay").addClass("enable");
-            $("#overlay").removeClass("disable")
-            var src = $(this).attr("src");
-            $("#overlay-img").attr("src", src);
+          var src = $(this).attr("src");
+          $("#overlay-img").attr("src", src);
         }
+      }
+      else {
+        $("#overlay").addClass("enable");
+        $("#overlay").removeClass("disable")
+        var src = $(this).attr("src");
+        $("#overlay-img").attr("src", src);
+      }
     });  
 
     $("#close-button").on("click", function() {
-        $("#overlay").addClass("disable");
-        $("#overlay").removeClass("enable");
+      $("#overlay").addClass("disable");
+      $("#overlay").removeClass("enable");
     });
 });
+
+function myFunction() {
+  var x = document.getElementById("menu");
+  if (x.className === "") {
+    x.className += "responsive";
+  } else {
+    x.className = "";
+  }
+}
